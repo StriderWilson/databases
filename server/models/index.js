37a3c64ccc2f7d9e93fe7f4367ad2c7078fd3298
StriderWilson <strider.w@berkeley.edu>
2019-11-14 12:32:39 -0800

    Set up get and post requests in models

--- server/models/index.js
+++ server/models/index.js
@@ -2,14 +2,65 @@ var db = require('../db');
 
 module.exports = {
   messages: {
-    get: function () {}, // a function which produces all the messages
-    post: function () {} // a function which can be used to insert a message into the database
+    get: function () {
+      // db.connection.connect((err) => {
+      //   if (err) {
+      //     throw err;
+      //   } else {
+      //     console.log('Connection success!!');
+      //   }
+      // });
+      db.connection.query('select * from messages', (err, data) => {
+        if (err) {
+          throw err;
+        } else {
+          console.log('Our message data:', data);
+        }
+      });
+      // db.connection.end((err) => {
+      //   if (err) {
+      //     throw err;
+      //   }
+      // });
+    }, // a function which produces all the messages
+    post: function () {
+      db.connection.query("insert into messages values (2, 'Hello all world!', '2019-11-13 13:00:00', 1);", (err, data) => {
+        if (err) {
+          throw err;
+        } else {
+          console.log('Our posted message data:', data);
+        }
+      });
+    } // a function which can be used to insert a message into the database
   },
 
   users: {
     // Ditto as above.
-    get: function () {},
-    post: function () {}
+    get: function () {
+      // db.connection.connect((err) => {
+      //   if (err) {
+      //     throw err;
+      //   } else {
+      //     console.log('Connection success!!');
+      //   }
+      // });
+      db.connection.query('select * from users', (err, data) => {
+        if (err) {
+          throw err;
+        } else {
+          console.log('Our users data:', data);
+        }
+      });
+      // db.connection.end((err) => {
+      //   if (err) {
+      //     throw err;
+      //   }
+      // });
+    },
+    post: function () { }
   }
 };
+module.exports.messages.post();
+module.exports.messages.get();
+module.exports.users.get();
 
